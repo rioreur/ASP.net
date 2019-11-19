01190873bea0ff897e05347e8ac3921c14a60e6f
rioreur <louis.schuck@gmail.com>
2019-11-19 12:03:59 +0100

    refacto code

--- src/Isen.DotnetCode/Program.cs
+++ src/Isen.DotnetCode/Program.cs
@@ -7,21 +7,27 @@ namespace Isen.DotnetCode
     {
         static void Main(string[] args)
         {
+            // Hello test
             Hello hello = new Hello("World");
             Console.WriteLine(hello.Great());
 
+            // MyCollection test d'initialisation
             var array = new String[] {"Je", "suis", "presque", "une", "phrase"};
             MyCollection myCollection = new MyCollection(array);
             Console.WriteLine(myCollection);
+            // MyCollection test de "Add"
             myCollection.Add("mais");
             myCollection.Add("pas");
             myCollection.Add("trop");
             Console.WriteLine(myCollection);
+            // MyCollection test de "RemoveAt"
             myCollection.RemoveAt(3);
             myCollection.RemoveAt(3);
             Console.WriteLine(myCollection);
+            // MyCollection test de "IndexOf"
             Console.WriteLine("index de \"phrase\" : " + myCollection.IndexOf("phrase"));
             Console.WriteLine("index de \"mais\" : " + myCollection.IndexOf("mais"));
+            // MyCollection test de l'accesseur indexer
             Console.WriteLine("troisième mot : " + myCollection[3]);
             myCollection[3] = "ou";
             Console.WriteLine("troisième mot : " + myCollection[3]);
